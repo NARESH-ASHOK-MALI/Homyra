e079a0cbd634f9ce49d62f8a0c6129d6feb4a739
NARESH ASHOK MALI <nareshashokmali@gmail.com>
2025-10-07 20:50:23 +0530

    Uploading phase 1 partc

--- app.js
+++ app.js
@@ -31,6 +31,16 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 app.engine("ejs", ejsMate);
 
+const validateListing=(req,res,next)=>{
+  let {error}=listingSchema.validate(req.body);
+  if(error){
+    let errMsg=error.details.map((el)=>el.message).join(",");
+    throw new ExpressError(400,errMsg);
+  }else{
+    next();
+  }
+}
+
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
@@ -54,12 +64,7 @@ app.get("/listings/:id",  wrapAsync(async (req, res) => {
 }));
 
 //Create Route
-app.post("/listings", wrapAsync(async (req, res, next) => {
-  let result=listingSchema.validate(req.body);
-  console.log(result);
-  if(result.error){
-    throw new ExpressError(400,result.error);
-  }
+app.post("/listings", validateListing ,wrapAsync(async (req, res, next) => {
   const newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
@@ -73,7 +78,7 @@ app.get("/listings/:id/edit",  wrapAsync(async (req, res ) => {
 }));
 
 //Update Route
-app.put("/listings/:id",  wrapAsync(async (req, res) => {
+app.put("/listings/:id", validateListing , wrapAsync(async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
